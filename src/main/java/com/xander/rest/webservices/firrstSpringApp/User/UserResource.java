4719795a4457ecc78e770371f4e71203d80e926d
Xander999 <sahasuraj420@gmail.com>
2021-10-30 16:09:50 +0530

    fourth commit

--- src/main/java/com/xander/rest/webservices/firrstSpringApp/User/UserResource.java
+++ src/main/java/com/xander/rest/webservices/firrstSpringApp/User/UserResource.java
@@ -1,13 +1,17 @@
 package com.xander.rest.webservices.firrstSpringApp.User;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 
 @RestController
 public class UserResource {
@@ -26,16 +30,28 @@ public class UserResource {
 	//GET /users/{id}
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
-		return service.findOne(id);		
+		User user = service.findOne(id);
+		if(user==null) {
+			throw new UserNotFoundException("id-"+id);
+		}
+		return user; 
 	}
 	
 	//CREATED
 	//Input - details of user
 	//Output - CREATED & Returrn the created URI
 	@PostMapping("/users")
-	public String createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		User savedUser = service.save(user);
-		return "User Saved Successfully";
+		//return "User Saved Successfully";
+		URI location = ServletUriComponentsBuilder
+		.fromCurrentRequest()
+		.path("/{id")
+		.buildAndExpand(savedUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).build(); 
+		
+		
 	}
 
 }
